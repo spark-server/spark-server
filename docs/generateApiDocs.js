1bf9183ec5a1ab45509ccb15cbd838efb327a0e8
tobilg <tobilg@users.noreply.github.com>
2016-03-21 13:24:12 +0100

    Fixed path issue when run via npm

--- docs/generateApiDocs.js
+++ docs/generateApiDocs.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
+var path = require('path');
 var raml2html = require('raml2html');
 var configWithDefaultTemplates = raml2html.getDefaultConfig();
 
-raml2html.render('../docs/spark-server.raml', configWithDefaultTemplates).then(function(result) {
+raml2html.render(path.join(__dirname + '/spark-server.raml'), configWithDefaultTemplates).then(function(result) {
 
-    fs.writeFile("../docs/spark-server.html", result, function(err) {
+    fs.writeFile(path.join(__dirname + '/spark-server.html'), result, function(err) {
         if(err) {
             console.log(err);
         }
-        console.log("raml2html successfully generated the API docs!");
+        console.log('raml2html successfully generated the API docs!');
     });
 }, function(err) {
     console.log(err);
